b238089f7374bfe7df3080b1fb79fbcf0c035a0c
Juliansv <juliansv22@gmail.com>
2024-12-10 07:25:18 +0100

    Create project

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //api/account
+    public class AccountController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Hi()
+        {
+            return "Hello from Account Controller";
+        }
+
+    }
+}
