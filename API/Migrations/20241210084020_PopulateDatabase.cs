501d56dab9bb1063ce434ce7faf919e3be2199df
Juliansv <juliansv22@gmail.com>
2024-12-10 11:03:16 +0100

    add register endpoint

---
+++ API/Migrations/20241210084020_PopulateDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+        table: "AspNetRoles",
+        columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+        values: new object[,]
+        {
+            { "1", "Admin", "ADMIN", Guid.NewGuid().ToString() },
+            { "2", "User", "USER", Guid.NewGuid().ToString() },
+        });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+        table: "AspNetRoles",
+        keyColumn: "Id",
+        keyValues: new object[] { "1", "2" });
+        }
+    }
+}
